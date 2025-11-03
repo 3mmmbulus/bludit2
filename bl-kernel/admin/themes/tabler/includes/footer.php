2cded6506343559442f675bd2d504e6b9f409732
3mmmbulus <your-email@example.com>
2025-11-03 04:14:29 +0000

    重构: 提取footer到独立文件并更新版权信息为Maigewan

--- bl-kernel/admin/themes/tabler/includes/footer.php
+++ bl-kernel/admin/themes/tabler/includes/footer.php
@@ -1,22 +1,19 @@
 <?php defined('BLUDIT') or die('Bludit CMS.'); ?>
 
 <!-- Footer -->
-<div class="text-center text-secondary mt-3">
-	<ul class="list-inline list-inline-dots mb-0">
-		<li class="list-inline-item">
-			<a href="https://maigewan.com" target="_blank" rel="noopener noreferrer" class="link-secondary">
-				© <?php echo date('Y') ?> Maigewan
-			</a>
-		</li>
-		<li class="list-inline-item">
-			<a href="https://maigewan.com/about" target="_blank" rel="noopener noreferrer" class="link-secondary">
-				<?php echo isset($L) ? $L->get('About') : 'About' ?>
-			</a>
-		</li>
-		<li class="list-inline-item">
-			<a href="https://maigewan.com/docs" target="_blank" rel="noopener noreferrer" class="link-secondary">
-				<?php echo isset($L) ? $L->get('Documentation') : 'Documentation' ?>
-			</a>
-		</li>
-	</ul>
-</div>
+<footer class="footer footer-transparent d-print-none">
+	<div class="container-xl">
+		<div class="row text-center align-items-center">
+			<div class="col-12 col-lg-auto mt-3 mt-lg-0">
+				<ul class="list-inline list-inline-dots mb-0">
+					<li class="list-inline-item">
+						© <?php echo date('Y') ?> <a href="https://maigewan.com" target="_blank" rel="noopener noreferrer" class="link-secondary">Maigewan</a>
+					</li>
+					<li class="list-inline-item">
+						版本号: <?php echo BLUDIT_VERSION ?>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
--- bl-kernel/admin/themes/tabler/index.php
+++ bl-kernel/admin/themes/tabler/index.php
@@ -109,22 +109,7 @@
 		</div>
 		
 		<!-- Footer -->
-		<footer class="footer footer-transparent d-print-none">
-			<div class="container-xl">
-				<div class="row text-center align-items-center">
-					<div class="col-12 col-lg-auto mt-3 mt-lg-0">
-						<ul class="list-inline list-inline-dots mb-0">
-							<li class="list-inline-item">
-								<?php echo (defined('BLUDIT_PRO')) ? 'BLUDIT PRO' : 'BLUDIT' ?>
-							</li>
-							<li class="list-inline-item">
-								Version <?php echo BLUDIT_VERSION ?>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</footer>
+		<?php include('includes/footer.php'); ?>
 	</div>
 </div>
 
